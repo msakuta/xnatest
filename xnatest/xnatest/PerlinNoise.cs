0ebb0fbb5c8443b2cdcf6fe965a94d1f72137111
msakuta <masahiro.sakuta@gmail.com>
2011-09-24 09:36:32 +0000

    Optimized perlin_noise a bit.

--- xnatest/xnatest/PerlinNoise.cs
+++ xnatest/xnatest/PerlinNoise.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace xnatest
 {
@@ -28,23 +25,24 @@ namespace xnatest
         public static void perlin_noise(long seed, PerlinNoiseCallback callback, long cellsize, int xofs = 0, int yofs = 0)
         {
             int[,] work = new int[cellsize, cellsize];
-            float[,] work2 = new float[cellsize, cellsize];
-            float maxwork2 = 0;
+            int[,] work2 = new int[cellsize, cellsize];
+            int maxwork2 = 0;
             int octave;
+            int xi, yi;
+
+            // Temporarily save noise patturn for use as the source signal of fractal noise.
+            for (xi = 0; xi < cellsize; xi++) for (yi = 0; yi < cellsize; yi++)
+                {
+                    Random rs = new Random((int)(seed ^ (xi + xofs) + ((yi + yofs) << 16)));
+                    int bas = rs.Next(255);
+                    work[xi, yi] = bas;
+                }
+
+            // Accumulate signal over octaves to produce Perlin noise.
             for (octave = 0; (1 << octave) < cellsize; octave += 1)
             {
                 int cell = 1 << octave;
-                int xi, yi;
                 int k;
-                for (xi = 0; xi < cellsize / cell; xi++) for (yi = 0; yi < cellsize / cell; yi++)
-                    {
-                        Random rs = new Random((int)(seed ^ (xi + xofs) + ((yi + yofs) << 16)));
-                        int bas = rs.Next(255);
-                        if (octave == 0)
-                            callback.call(bas, xi, yi);
-                        else
-                            work[xi, yi] = bas;
-                    }
                 if (octave == 0)
                 {
                     for (xi = 0; xi < cellsize; xi++) for (yi = 0; yi < cellsize; yi++)
@@ -53,24 +51,26 @@ namespace xnatest
                 else for (xi = 0; xi < cellsize; xi++) for (yi = 0; yi < cellsize; yi++)
                         {
                             int xj, yj;
-                            double[] sum = new double[4] { 0, 0, 0, 0 };
+                            double sum = 0;
                             for (k = 0; k < 1; k++)
                             {
                                 for (xj = 0; xj <= 1; xj++) for (yj = 0; yj <= 1; yj++)
                                     {
-                                        sum[k] += (double)work[xi / cell + xj, yi / cell + yj]
+                                        sum += (double)work[xi / cell + xj, yi / cell + yj]
                                             * (xj != 0 ? xi % cell : (cell - xi % cell - 1)) / (double)cell
                                             * (yj != 0 ? yi % cell : (cell - yi % cell - 1)) / (double)cell;
                                     }
-                                work2[xi, yi] = work2[xi, yi] + (float)sum[k];
+                                work2[xi, yi] += (int)sum;
                                 if (maxwork2 < work2[xi, yi])
                                     maxwork2 = work2[xi, yi];
                             }
                         }
             }
-            for (int xi = 0; xi < cellsize; xi++) for (int yi = 0; yi < cellsize; yi++)
+
+            // Return result
+            for (xi = 0; xi < cellsize; xi++) for (yi = 0; yi < cellsize; yi++)
                 {
-                    callback.call(work2[xi, yi] / maxwork2, xi, yi);
+                    callback.call((float)work2[xi, yi] / maxwork2, xi, yi);
                 }
         }
     }
